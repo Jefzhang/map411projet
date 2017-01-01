d841bd079f66426f6c5ce4383fa5a9d84c324179
Jianfei ZHANG <jfzhang@arandir.eleves.polytechnique.fr>
2017-01-01 15:27:42 +0100

    add other schemas for model 1.1

--- convection_uni_dim.py
+++ convection_uni_dim.py
@@ -15,7 +15,7 @@ lg = 50       # intervalle en x=[0,lg]
 dx = 0.1      # dx = pas d'espace
 dt = 0.025    # dt = pas de temps
 Tfinal = 5   # Temps final souhaite
-Vitesse = 1   # Vitesse de difusion
+Vitesse = 2   # Vitesse de difusion
 Coeffi = 1    #Coefficient de difusion
 xmean = 20
 sigma = 1
@@ -54,12 +54,19 @@ plt.close(fig)
 
 # Initialize u by the initial data u0
 uexpcen = u0.copy()
+uexpdecen = u0.copy()
+ucrankni = u0.copy()
 #print(len(uexpcen))
 
 # Construction de la matrice (creuse) pour le schema explicite cnetree
 Kc = sp.sparse.diags([-1/(2*dx),0,+1/(2*dx)],[-1,0,1],shape=(nx,nx))
 Aexpcen = sp.sparse.identity(nx) - Vitesse*dt*Kc
 #print(Aexpcen.shape)
+Kd = sp.sparse.diags([-1/dx,1/dx],[-1,0],shape=(nx,nx))
+Aexpdecen = sp.sparse.identity(nx) - Vitesse*dt*Kd
+
+Acrank1 = sp.sparse.identity(nx) + Vitesse*dt* Kc/2
+Acrank2 = sp.sparse.identity(nx) - Vitesse*dt* Kc/2
 
 # Nombre de pas de temps effectues
 nt = int(Tfinal/dt)
@@ -69,13 +76,29 @@ Tfinal = nt*dt # on corrige le temps final (si Tfinal/dt n'est pas entier
 # Time loop
 for n in range(1,nt+1):
     uexpcen[1:nx+1] = Aexpcen * uexpcen[1:nx+1]
+    uexpdecen[1:nx+1] = Aexpdecen * uexpdecen[1:nx+1]
+    ucrankni[1:nx+1] = sp.sparse.linalg.spsolve(Acrank1,Acrank2*ucrankni[1:nx+1])
+
 
    # Print solution
     if n%5 == 0:
         plt.figure(2)
         plt.clf()
-        #plt.subplot(121)
+
+        plt.subplot(221)
         plt.plot(x,u0,'b',x,uexpcen,'r')
         plt.xlabel('$x$')
         plt.title('Schema explicite centree')
+
+        plt.subplot(222)
+        plt.plot(x,u0,'b',x,uexpdecen,'r')
+        plt.xlabel('$x$')
+        plt.title('Schema explicite decentree amont')
+
+        plt.subplot(223)
+        plt.plot(x,u0,'b',x,ucrankni,'r')
+        plt.xlabel('$x$')
+        plt.title('Schema Crank-Nicholson')
+
+
         plt.pause(0.1)
